3f85be5500afe1cdb7e52a3586531fa584ab9677
Robert Owens <me@robertowens.dev>
2021-04-09 15:05:22 -0500

    Initial project template

---
+++ software-assignment/project.clj
@@ -0,0 +1,17 @@
+(defproject software-assignment "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"] 
+                 [ultra-csv "0.2.3"] 
+                 [http-kit "2.5.3"] 
+                 [compojure "1.6.1"] 
+                 [ring/ring-codec "1.1.3"] 
+                 [org.clojure/data.json "2.0.2"]]
+  :main ^:skip-aot guaranteed-rate-record-parser-and-retrieval-system.core
+  :target-path "target/%s"
+  :plugins [[lein-cljfmt "0.6.1"]]
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+
---
+++ software-assignment/src/software_assignment/core.clj
@@ -0,0 +1,7 @@
+(ns software-assignment.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
