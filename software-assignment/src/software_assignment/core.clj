1e2c02bd38a731079b9a09f1be3aa04d556c12c8
Robert Owens <me@robertowens.dev>
2021-04-09 15:07:44 -0500

    Adds function to retrieve delimiter used in a csv entry.

--- software-assignment/src/software_assignment/core.clj
+++ software-assignment/src/software_assignment/core.clj
@@ -1,5 +1,17 @@
 (ns software-assignment.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.string]))
+
+(defn get-delimiter
+  [csv-record]
+  (if (or (clojure.string/includes? csv-record "|") (clojure.string/includes? csv-record "%7C"))
+    "\\|"
+    (if (or (clojure.string/includes? csv-record ",") (clojure.string/includes? csv-record "%2C"))
+      ","
+      (if (or (clojure.string/includes? csv-record " ") (clojure.string/includes? csv-record "+"))
+        " "
+        (println "Unsported delimiter.")))))
+
 
 (defn -main
   "I don't do a whole lot ... yet."
