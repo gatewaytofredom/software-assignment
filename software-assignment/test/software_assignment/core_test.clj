e2a25c0f5f8cbcd2043e6fb9792a5fc84bf9491c
Robert Owens <me@robertowens.dev>
2021-04-09 15:30:15 -0500

    Adds test for sort-last-name.

--- software-assignment/test/software_assignment/core_test.clj
+++ software-assignment/test/software_assignment/core_test.clj
@@ -7,6 +7,10 @@
    ["Chen" "Evans" "evans@email" "green" "2001-01-02"] ["Zim" "Apple" "zim@email" "brown" "1980-01-01"] 
    ["A" "john" "zim@email" "pink" "1000-02-02"]])
 
+(def test-sort-vector
+  [["Washington" "George" "Washington@email.com" "Green" "1732-2-22"] 
+   ["Adams" "John" "Adams@email.com" "Red" "1735-10-30"]])
+
 (deftest get-delimiter-test
   (testing "Function should return a string literal of the detected delimiter in a csv file.")
   (is (= "\\|"
@@ -16,3 +20,8 @@
   (testing "Function should return a 2d vector where each element represents one entry in a csv file.")
   (is (= expected-2d-vector
          (csv-to-2d-vector "resources/test.csv"))))
+
+(deftest sort-last-name-test
+  (testing "Function should return vector containing vectors sorted last name decending.")
+  (is (= [["Adams" "John" "Adams@email.com" "Red" "1735-10-30"] ["Washington" "George" "Washington@email.com" "Green" "1732-2-22"]])
+      (sort-last-name test-sort-vector)))
